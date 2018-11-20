74303ae1a10e00832bd57970ed324ac798865236
Prabhu <prabhu.wali@gmail.com>
2018-11-20 17:58:23 +0000

    final commit

--- app.js
+++ app.js
@@ -7,7 +7,13 @@ function getBlock(index) {
   	//Add your code here
   	// Start by requesting the hash
   	// Then, request the block and use console.log.
-  
+  be.blockIndex(0).then(blockData =>{
+    be.block(JSON.parse(blockData).blockHash).then(block => {
+		console.log(block)    
+    })
+  }).catch(e => {
+  	throw e
+  })
 }
 
 (function theLoop (i) {
